db590682251f2a019bf4fe84a7d8b38d0ef044b0
whguddn <hataraci9350@daum.net>
2025-12-23 19:54:32 +0900

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import root from './router/root';
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import root from "./router/root";
 
 function App() {
-  return (
-    <RouterProvider router={root}></RouterProvider>
-  );
+  return <RouterProvider router={root}></RouterProvider>;
 }
 
 export default App;
