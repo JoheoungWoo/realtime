ec31efa7f0129b37ca05c8ddbb86045391e6c319
whguddn <hataraci9350@daum.net>
2025-12-23 19:02:16 +0900

    first commit

---
+++ src/router/root.js
@@ -0,0 +1,70 @@
+import { lazy, Suspense } from "react";
+
+import Basiclayout from "../layouts/Basiclayout";
+import aboutGreenRouter from "./aboutGreenRouter";
+import academicSupportRouter from "./academicSupportRouter";
+import admissionEducationRouter from "./admissionEducationRouter";
+import campusLifeRouter from "./campusLifeRouter";
+import informationRouter from "./informationRouter";
+import accountRouter from "./accountRouter";
+import adminRouter from "./adminRouter";
+import RealTimeSensor from "../components/RealTimeSensor";
+
+const { createBrowserRouter } = require("react-router-dom");
+const Loading = <div>Loading......</div>;
+
+const Main = lazy(() => import("../pages/MainPage"));
+
+const root = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={Loading}>
+        <Basiclayout children={<Main />}></Basiclayout>
+      </Suspense>
+    ),
+  },
+  {
+    path: "aboutgreen",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    // element: <Suspense fallback={Loading}><Main /></Suspense>,
+    children: aboutGreenRouter(),
+  },
+  {
+    path: "academicsupport",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    children: academicSupportRouter(),
+  },
+  {
+    path: "admissioneducation",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    children: admissionEducationRouter(),
+  },
+  {
+    path: "campuslife",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    children: campusLifeRouter(),
+  },
+  {
+    path: "information",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    children: informationRouter(),
+  },
+  {
+    path: "account",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    children: accountRouter(),
+  },
+  {
+    path: "admin",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    children: adminRouter(),
+  },
+  {
+    path: "v",
+    // element: <Suspense fallback={Loading}><Basiclayout children={<Main />}></Basiclayout></Suspense>,
+    element: <RealTimeSensor />,
+  },
+]);
+
+export default root;
